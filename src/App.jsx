617eab648fee0dd66f1892c9705691cd84dc7692
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-03-16 18:02:37 +0530

    placement dashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <h1 className='text-red-500 text-5xl font-extrabold'>Placement Dashboard</h1>
+   </div>
+  )
+}
+
+export default App
