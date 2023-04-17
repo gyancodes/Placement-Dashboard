8a4c82c60dfcfce6c0c74e694ff6b44fcf44b420
Avinash85kr <112809709+Avinash85kr@users.noreply.github.com>
2023-04-17 16:19:47 +0530

    minor changes

--- src/components/pages/UnplacedData/Totalintrested&Placed.jsx
+++ src/components/pages/UnplacedData/Totalintrested&Placed.jsx
@@ -6,46 +6,46 @@ import { Bar } from '@ant-design/plots';
 const InterestedStudentPlaced = () => {
   const data = [
     {
-
-        'Interested': 1179,
-
-        'UnPlaced': 1006,
+        
+        'Interested': 1165,
+       
+        'Placed': 394,
          school: "BBSR",
       },
-
+     
       {
-
+        
         'Interested': 140,
-
-         'UnPlaced': 97,
+       
+         'Placed': 43,
          school: "BLS",
       },
       {
-
+        
         'Interested': 814,
-
-        'UnPlaced' : 314,
+       
+        'Placed' : 500,
          school: "PKD",
       },
       {
-
-        'Interested': 108,
-
-       'UnPlaced': 99,
+       
+        'Interested': 122,
+       
+       'Placed': 17,
         school: "BLGR",
       },
       {
-
-        'Interested': 103,
-
-        'UnPlaced': 65,
+       
+        'Interested': 107,
+       
+        'Placed': 42,
         school: "VIZAG",
       },
       {
-
+       
         'Interested': 121,
-
-        'UnPlaced': 88,
+       
+        'Placed': 33,
         school: "RGD",
       },
   ];
@@ -54,30 +54,30 @@ const InterestedStudentPlaced = () => {
     xField: ["Interested"],
     yField: 'school',
     seriesField: 'school',
-
+   
     legend: {
       position: 'top-left',
     },
   };
   const un = {
     data,
-    xField: ["UnPlaced"],
+    xField: ["Placed"],
     yField: 'school',
     seriesField: 'school',
-
+   
     legend: {
       position: 'top-left',
     },
   };
-
-  return(
+  
+  return( 
     <>
     <Title className='text-cyan-400 text-3xl font-bold'>This in Intrested student graph</Title>
-   <Bar {...config} height={300}/ >
-<Title className='text-cyan-400 text-3xl font-bold'>This in unplaced student graph</Title>
-  <Bar {...un} height={300} /></>);
-
-
+   <Bar {...config}/>
+<Title className='text-cyan-400 text-3xl font-bold'>This in Placed student graph</Title>
+  <Bar {...un} /></>);
+  
+ 
 };
 
 export default InterestedStudentPlaced;
